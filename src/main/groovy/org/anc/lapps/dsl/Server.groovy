3dcc1b5a55426f74eb15fa8c23ea302e21aa1ffd
Keith Suderman <suderman@anc.org>
2014-02-13 11:33:46 -0500

    Removed some dead code.

--- src/main/groovy/org/anc/lapps/dsl/Server.groovy
+++ src/main/groovy/org/anc/lapps/dsl/Server.groovy
@@ -4,7 +4,6 @@ package org.anc.lapps.dsl
  * @author Keith Suderman
  */
 class Server {
-//    String name
     String url
     String username
     String password
@@ -12,8 +11,7 @@ class Server {
     public Server() { }
 
     public Server(ServerDelegate delegate) {
-        println "Creating server for ${delegate.url}"
-//        this.name = delegate.name
+//        println "Creating server for ${delegate.url}"
         this.url = delegate.url
         this.username = delegate.username
         this.password = delegate.password
